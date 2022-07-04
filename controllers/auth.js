03c1b58cec7fa1b5d3e92b53907dc4afdbf70dbb
nntemkas <nntemkas@ots.gr>
2022-07-04 21:44:11 +0300

    REST DONE

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,41 +6,36 @@ const {throwValidationErrors, commonErrorHandling, resourceNotFound} = require('
 const {operationSuccess} = require('../util/common_reposnses');
 
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
     throwValidationErrors(req);
-
-    bcrypt.hash(req.body.password, 12)
-        .then(hashedPsw => {
-            const user = new User({
-                email: req.body.email,
-                name: req.body.name,
-                password: hashedPsw
-            });
-            return user.save();
-        })
-        .then(result => {
-            return operationSuccess(res, {message: 'User Created'})
-        })
-        .catch(error => commonErrorHandling(error, next));
+    try {
+        const hashedPsw = bcrypt.hash(req.body.password, 12)
+        const user = new User({
+            email: req.body.email,
+            name: req.body.name,
+            password: hashedPsw
+        });
+        await user.save();
+        return operationSuccess(res, {message: 'User Created'});
+    } catch (error) {
+        commonErrorHandling(error, next)
+    }
 }
-exports.login = (req, res, next) => {
-    let loadedUser;
-    User.findOne({email: req.body.email})
-        .then((user) => {
-            resourceNotFound(user, next, 'User', 'Wrong E-mail or password');
-            loadedUser = user;
-            return bcrypt.compare(req.body.password, user.password)
-        })
-        .then((result) => {
-            resourceNotFound(result, next, 'User', 'Wrong E-mail or password');
-            const token = jwt.sign({
-                    email: loadedUser.email,
-                    userId: loadedUser._id.toString()
-                },
-                'secret',
-                {expiresIn: '1h'}
-            );
-            return operationSuccess(res, {token: token, userId: loadedUser._id.toString()});
-        })
-        .catch(error => commonErrorHandling(error, next))
+exports.login = async (req, res, next) => {
+    try {
+        const user = await User.findOne({email: req.body.email})
+        resourceNotFound(user, next, 'User', 'Wrong E-mail or password');
+        const verifiedPassword = await bcrypt.compare(req.body.password, user.password)
+        resourceNotFound(verifiedPassword, next, 'User', 'Wrong E-mail or password');
+        const token = jwt.sign({
+                email: user.email,
+                userId: user._id.toString()
+            },
+            'secret',
+            {expiresIn: '1h'}
+        );
+        return operationSuccess(res, {token: token, userId: user._id.toString()});
+    }catch(error){
+        commonErrorHandling(error, next);
+    }
 };
